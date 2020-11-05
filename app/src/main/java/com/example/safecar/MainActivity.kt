f48d20f43cfaa31fa1bb96b296c922f9769e4481
Pedro Varela <pedrovarela999@gmail.com>
2020-11-05 14:41:28 +0000

    Primeiras Mudanças na aplicação Criação da BottomNavigationBar e de alguns Fragmentos

--- app/src/main/java/com/example/safecar/MainActivity.kt
+++ app/src/main/java/com/example/safecar/MainActivity.kt
@@ -2,11 +2,68 @@ package com.example.safecar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var homeFragment: HomeFragment
+    lateinit var gpsFragment: GPSFragment
+    lateinit var userFragment: UserFragment
+    lateinit var settingsFragment: SettingsFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView : BottomNavigationView = findViewById(R.id.bot_nav)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+
+            when (item.itemId) {
+
+                R.id.home -> {
+
+                    homeFragment = HomeFragment()
+                    supportFragmentManager
+                            .beginTransaction()
+                            .replace(R.id.frame_layout, homeFragment)
+                            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                            .commit()
+                }
+
+                R.id.gps -> {
+
+                    gpsFragment = GPSFragment()
+                    supportFragmentManager
+                            .beginTransaction()
+                            .replace(R.id.frame_layout, gpsFragment)
+                            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                            .commit()
+                }
+
+                R.id.user -> {
+
+                    userFragment = UserFragment()
+                    supportFragmentManager
+                            .beginTransaction()
+                            .replace(R.id.frame_layout, userFragment)
+                            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                            .commit()
+                }
+
+                R.id.settings -> {
+
+                    settingsFragment = SettingsFragment()
+                    supportFragmentManager
+                            .beginTransaction()
+                            .replace(R.id.frame_layout, settingsFragment)
+                            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                            .commit()
+                }
+            }
+
+            true
+        }
     }
 }
-//Testetetetete
